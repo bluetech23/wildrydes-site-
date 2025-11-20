213b6a95d432a1eaebc7c38bcd144c19f97d7625
Raaga <139925751+bluetech23@users.noreply.github.com>
2025-11-20 00:48:37 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_2b6qHESxv', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolClientId: '7gaba90cp85bsnemhgm8d55bbj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
